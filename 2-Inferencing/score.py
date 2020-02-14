6acb3b30a4b026d7377154d4400081d860ae5ed6
phillipf <phillip-f@live.com>
2020-02-14 15:14:08 +1100

    addition of blank check when model run

--- 2-Inferencing/score.py
+++ 2-Inferencing/score.py
@@ -56,40 +56,42 @@ def init():
 
 
 def run(raw_data):
+    if json.loads(raw_data)['text']:
+        # Encode inputs using tokenizer
+        inputs = tokenizer.encode_plus(
+            json.loads(raw_data)['text'],
+            add_special_tokens=True,
+            max_length=max_seq_length
+        )
+        input_ids, token_type_ids = inputs["input_ids"], inputs["token_type_ids"]
 
-    # Encode inputs using tokenizer
-    inputs = tokenizer.encode_plus(
-        json.loads(raw_data)['text'],
-        add_special_tokens=True,
-        max_length=max_seq_length
-    )
-    input_ids, token_type_ids = inputs["input_ids"], inputs["token_type_ids"]
+        # The mask has 1 for real tokens and 0 for padding tokens.
+        # Only real tokens are attended to.
+        attention_mask = [1] * len(input_ids)
 
-    # The mask has 1 for real tokens and 0 for padding tokens.
-    # Only real tokens are attended to.
-    attention_mask = [1] * len(input_ids)
+        # Zero-pad up to the sequence length.
+        padding_length = max_seq_length - len(input_ids)
+        input_ids = input_ids + ([0] * padding_length)
+        attention_mask = attention_mask + ([0] * padding_length)
+        token_type_ids = token_type_ids + ([0] * padding_length)
 
-    # Zero-pad up to the sequence length.
-    padding_length = max_seq_length - len(input_ids)
-    input_ids = input_ids + ([0] * padding_length)
-    attention_mask = attention_mask + ([0] * padding_length)
-    token_type_ids = token_type_ids + ([0] * padding_length)
+        # Make prediction
+        predictions = model.predict({
+            'input_ids': tf.convert_to_tensor([input_ids], dtype=tf.int32),
+            'attention_mask': tf.convert_to_tensor(
+                [attention_mask],
+                dtype=tf.int32),
+            'token_type_ids': tf.convert_to_tensor(
+                [token_type_ids],
+                dtype=tf.int32)
+        })
 
-    # Make prediction
-    predictions = model.predict({
-        'input_ids': tf.convert_to_tensor([input_ids], dtype=tf.int32),
-        'attention_mask': tf.convert_to_tensor(
-            [attention_mask],
-            dtype=tf.int32),
-        'token_type_ids': tf.convert_to_tensor(
-            [token_type_ids],
-            dtype=tf.int32)
-    })
+        result = {
+            'prediction': str(labels[predictions[0].argmax().item()]),
+            'probability': str(predictions[0].max())
+        }
 
-    result = {
-        'prediction': str(labels[predictions[0].argmax().item()]),
-        'probability': str(predictions[0].max())
-    }
-
-    print(result)
+        print(result)
+    else:
+        result = "None"
     return result
